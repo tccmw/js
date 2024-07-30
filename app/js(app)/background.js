aac0b1e7e4c7737c5236820139a085355f6c77d3
tccmw <jungmwoo6248@gmail.com>
2024-07-30 13:38:02 +0900

    background

--- app/js(app)/background.js
+++ app/js(app)/background.js
@@ -1,9 +1,9 @@
-const images = ["img 1.jpg", "img 2.jpg", "img 3.jpg"];
+const images = ["1.jpg", "2.jpg", "3.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+bgImage.src = `/JAVASCRIPT/app/img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+document.body.append(bgImage);
