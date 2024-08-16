d546ef98ced8b51f709195583e9019679456ae66
tccmw <jungmwoo6248@gmail.com>
2024-08-16 20:06:21 +0900

    refresh

--- paint/board.js
+++ paint/board.js
@@ -1,29 +1,27 @@
-const main = document.querySelector("main");
-const canvas = document.querySelector("#Input");
-const ctn = canvas.getContext('2D');
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 800;
 
-ctn.fillText = "white";
-ctn.fillText(0,0,canvas.width, canvas.height);
+ctx.fillRect(200,200,50,200);
+ctx.fillRect(400,200,50,200);
+ctx.lineWidth = 5;
+ctx.strokeRect(300,300,50,100);
+ctx.fillRect(200,200,200,20);
+ctx.moveTo(200,200);
+ctx.lineTo(325,100);
+ctx.lineTo(450,200);
+ctx.fill();
 
-canvas.addEventListener("contextmenu", e => {
-  e.preventDefault();
-})
+ctx.beginPath();
+ctx.fillRect(200,600,30,100);
+ctx.fillRect(400,600,30,100);
+ctx.fillRect(280,600,70,200);
+ctx.arc(315,530,50,0,2*Math.PI);
+ctx.fill();
 
-const.btns = document.querySelector("#change li");
-
-Array.
-
-paint.addEventListener("click", () =>{
-  if(paint.innerText== 'PAINT MINT'){
-    canvas.style.backgroundcolor = "aquamarine";
-    ctn.fillText = "aquamarine"; 
-    ctn.fillText(0,0,canvas.width, canvas.height);
-    paint.innerText = 'PAINT WHITE';
-  }
-  else{
-    canvas.style.backgroundcolor = "white";
-    ctn.fillText = "white"; 
-    ctn.fillText(0,0,canvas.width, canvas.height);
-    paint.innerText = 'PAINT MINT';
-  }
-  })
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(295,515,5,1*Math.PI,2*Math.PI);
+ctx.arc(340,515,5,1*Math.PI,2*Math.PI);
+ctx.fill();
