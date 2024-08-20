ff108041823953f10b376bcd7b9f622cdd3df004
tccmw <jungmwoo6248@gmail.com>
2024-08-20 16:19:00 +0900

    move

--- paint/clickmove.js
+++ paint/clickmove.js
@@ -1,3 +1,5 @@
+const colorOption = Array.from(document.getElementsByClassName("color-option"));
+const color = document.querySelector("#color");
 const lineWidth = document.querySelector("#line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -23,13 +25,26 @@ function onMouseDown(){
 function onMouseUp(){
   Mouse = false;
   ctx.beginPath();
-}
+} 
 
 function lineWidthchage(event){
   ctx.lineWidth = event.target.value;
+}
+
+function onColorChange(event){
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event){
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = ctx.fillStyle = color.value = colorValue;
 }
 
 canvas.addEventListener("mousemove",onMove);
 canvas.addEventListener("mousedown",onMouseDown);
 document.addEventListener("mouseup",onMouseUp);
 document.addEventListener("change",lineWidthchage);
+document.addEventListener("change",onColorChange);
+
+colorOption.forEach((color) => color.addEventListener("click",onColorClick));
