95b5dbbb2b3b10af149639706f9c5f262affc9a8
tccmw <jungmwoo6248@gmail.com>
2024-08-20 20:07:17 +0900

    sadh

--- paint/clickmove.js
+++ paint/clickmove.js
@@ -1,13 +1,19 @@
+const raectBnt = document.querySelector("#react-but");
+const destroyBnt = document.querySelector("#destroy-but");
+const fillBnt = document.querySelector("#fill-but");
 const colorOption = Array.from(document.getElementsByClassName("color-option"));
-const color = document.querySelector("#color");
+const color = document.querySelector("#line-color");
 const lineWidth = document.querySelector("#line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 800;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
 
 let Mouse = false;
+let isFilling = false;
 
 function onMove(event){
   if(Mouse){
@@ -37,14 +43,46 @@ function onColorChange(event){
 }
 
 function onColorClick(event){
-  const colorValue = event.target.dataset.color;
-  ctx.strokeStyle = ctx.fillStyle = color.value = colorValue;
+  const colorvalue = event.target.dataset.color;
+  ctx.strokeStyle = colorvalue;
+  ctx.fillStyle = colorvalue;
+  color.value = colorvalue;
+}
+
+function onFillClick(){
+  if(isFilling){
+    isFilling = false;
+    fillBnt.innerText = "Fill";
+  }
+  else{
+    isFilling = true;
+    fillBnt.innerText = "Draw";
+  }
+}
+
+function onClickFill(){
+  if(isFilling){
+    ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+  }
+}
+
+function onDestroyClick(event){
+  ctx.fillStyle = "white";
+  ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+}
+
+function onReactClick(){
+  ctx.strokeStyle = "white";
+  fillBnt.innerText = "Fill";
 }
 
 canvas.addEventListener("mousemove",onMove);
 canvas.addEventListener("mousedown",onMouseDown);
+canvas.addEventListener("mousedown",onClickFill);
 document.addEventListener("mouseup",onMouseUp);
 document.addEventListener("change",lineWidthchage);
 document.addEventListener("change",onColorChange);
-
+fillBnt.addEventListener("click",onFillClick);
 colorOption.forEach((color) => color.addEventListener("click",onColorClick));
+destroyBnt.addEventListener("click",onDestroyClick);
+raectBnt.addEventListener("click",onReactClick);
