151836d828a26c6d036aa968f756b2f457892e52
정명우 <163850138+tccmw@users.noreply.github.com>
2024-08-30 23:57:51 +0900

    Delete app directory

--- app/js(app)/background.js
+++
@@ -1,9 +0,0 @@
-const images = ["1.jpg", "2.jpg", "3.jpg"];
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-const bgImage = document.createElement("img");
-
-bgImage.src = `/JAVASCRIPT/app/img/${chosenImage}`;
-
-document.body.append(bgImage);
